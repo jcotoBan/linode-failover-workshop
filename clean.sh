bb23839a2565a550a6181477a669f991b3c63d45
jcotoBan <jcotsan@gmail.com>
2022-09-08 15:35:42 -0600

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cd ~
-kubectl delete svc nginx-workshop-service
-terraform destroy
+
+IFS=$'\n' contexts=($(kubectl config get-contexts --output=name))
+
+  for each in "${contexts[@]}"
+   do
+    kubectl config use-context $each
+    kubectl delete svc 
+    sleep 5
+   done
+
