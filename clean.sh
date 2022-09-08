d261a79da0b9e1a4092db08985dee0e0bb1c25ab
jcotoBan <jcotsan@gmail.com>
2022-09-08 15:36:09 -0600

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -5,7 +5,7 @@ IFS=$'\n' contexts=($(kubectl config get-contexts --output=name))
   for each in "${contexts[@]}"
    do
     kubectl config use-context $each
-    kubectl delete svc 
+    kubectl delete svc nginx-workshop-service
     sleep 5
    done
 
